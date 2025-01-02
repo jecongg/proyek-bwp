35ade7850d740e93bb5a4e25c0f3f87c95766df2
jecongg <160046911+jecongg@users.noreply.github.com>
2025-01-02 13:23:18 +0700

    ubah dikit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -237,9 +237,6 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">Login</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
-                        </li>
                     @endauth
                 </ul>
             </div>
